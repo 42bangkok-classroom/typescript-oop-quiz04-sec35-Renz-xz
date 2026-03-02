aafa3dbdfc6aab1f0694770f2b9e50f4c598b230
Woranon <68010980@kmitl.ac.th>
2026-03-02 09:48:59 +0700

    test

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,7 +6,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  test(): string[] {
-    return [];
+  test(): string []{
+    return this.userService.test();
   }
 }
