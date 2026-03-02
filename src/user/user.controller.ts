942fbd71277f68520e7fcf06552a5b2384c1f5ab
Woranon <68010980@kmitl.ac.th>
2026-03-02 10:12:06 +0700

    test

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,8 +5,8 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('/users')
+  @Get()
   test(): string []{
-    return this.userService.test();
+    return [];
   }
 }
